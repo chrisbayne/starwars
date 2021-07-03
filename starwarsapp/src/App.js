8b4391f395fccd8fb6a99b45b1cbf8a1bd53d903
Chris Bayne <chriss.bayne@gmail.com>
2021-07-02 18:30:49 -0700

    Adding in state variables and callback functions

--- starwarsapp/src/App.js
+++ starwarsapp/src/App.js
@@ -1,7 +1,24 @@
-import './App.css';
-import React, { useState } from 'react'
+import './App.css'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  // state variables
+  const [films, setFilms] = useState([]) 
+  const [loading, setLoading] = useState(true)
+
+  // Passing in two callback functions. Second callback function
+  // is an empty dependency array since the effect is only called once
+  // the component mounts to the DOM - it's only called once
+  useEffect(() =>{
+    async function fetchFilms() {
+      // fetching data for each film
+      let res = await fetch('https://swapi.dev/api/films/?format=json')
+      // creating data variable to receive data back in that variable
+      let data = res.json()
+      setFilms(data.results)
+    }
+  }, [])
+
   return (
     <div className="App">
       <h1>Star Wars App</h1>
